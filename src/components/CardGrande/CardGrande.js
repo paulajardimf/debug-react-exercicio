2c8e7e1452dc1bf829b7e88c3dbc9a355482a5a2
Paula <paulajardimf@gmail.com>
2022-10-24 15:15:20 -0300

    exercicio finalizado

--- src/components/CardGrande/CardGrande.js
+++ src/components/CardGrande/CardGrande.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import styled from 'styled components'
+import {LayoutCardGrande, ImgCardGrande, TituloCardGrande, InfosCardGrande} from './styles'
 
 
 function CardGrande(props) {
-   
     return (
         <LayoutCardGrande>
             <ImgCardGrande src={ props.imagem } />
             <InfosCardGrande>
                 <TituloCardGrande>{ props.nome }</TituloCardGrande>
-                <p>{ props.perfil.descricao }</p>
+                <p>{ props.descricao }</p>
             </InfosCardGrande>
         </LayoutCardGrande>
     )
 }
+
+export default CardGrande
